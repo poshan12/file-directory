dd02718951f6b281c9760963d3e3a907011afad1
poshan <poshankumar458@gmail.com>
2025-02-28 17:19:06 +0530

    first commit

--- file-directory/src/App.jsx
+++ file-directory/src/App.jsx
@@ -4,32 +4,78 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const fileStructure = [
+    {
+      name: 'Folder1',
+      type: 'folder',
+      children: [
+        { name: 'file1', type: 'file', },
+        { name: 'file1', type: 'file', },
+        { name: 'file1', type: 'file', },
+        { name: 'file1', type: 'file', },
+        {
+          name: 'folder2',
+          type: 'folder',
+          children: [
+            { name: 'file1', type: 'file', },
+            { name: 'file1', type: 'file', },
+            { name: 'file1', type: 'file', },
+            { name: 'file1', type: 'file', },
+
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Folder1',
+      type: 'folder',
+      children: [
+        { name: 'file1', type: 'file', },
+        { name: 'file1', type: 'file', },
+        { name: 'file1', type: 'file', },
+        { name: 'file1', type: 'file', },
+        // {
+        //   name: 'folder2',
+        //   type: 'folder',
+        //   children: [
+        //     { name: 'file1', type: 'file', },
+        //     { name: 'file1', type: 'file', },
+        //     { name: 'file1', type: 'file', },
+        //     { name: 'file1', type: 'file', }
+        //   ]
+        // }
+      ]
+    }
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {
+        fileStructure?.map((data) => {
+          return (
+            <Direcptry node={data} />
+          )
+        })
+      }
+    </div>
   )
 }
 
+const Direcptry = ({ node}) => {
+  console.log(node);
+  const [isopen, setOpen] = useState(false)
+  return (
+    <div style={{ marginLeft: '5px' }}>
+      {
+        node?.type !== 'folder' ? <p style={{ marginLeft: '5px' }}> {node?.name}</p> :
+          <div style={{ marginLeft: '5px' }}>
+            <p onClick={()=>setOpen(!isopen)}>{isopen ? '📂' :'📂'}{node?.name}</p>
+            { isopen && node?.children?.map((data) => {
+              return <Direcptry node={data} />
+            })}
+          </div>
+      }
+    </div>
+  )
+}
 export default App
